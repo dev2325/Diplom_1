dea1e4cb2141f2bcd29e809c371fbbb021b3dc39
Roman Pupkov <r2325@ya.ru>
2022-11-11 04:06:49 +0300

    Diplom_1 ver2 fixed

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -9,13 +9,13 @@ public class BunTest {
 
     @Test
     public void bunGetNameTest() {
-        // проверим что бургеру установилось нужное имя
+        // проверим что булке установилось нужное имя
         Assert.assertEquals("Ожидаемое имя не соответствует фактическому", "Bun Test Name", bun.getName());
     }
 
     @Test
     public void bunGetPriceTest() {
-        // проверим что бургеру установилась нужная цена
+        // проверим что булке установилась нужная цена
         Assert.assertEquals("Ожидаемая цена не соответствует фактической", 100, bun.getPrice(), 0);
     }
 }
